b8e162d23e35bf506f4fff5cd8732dcc9d1e6c15
FedorKorz <fedorkorz@gmail.com>
2023-06-25 17:43:30 +0300

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,48 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "cinemaAndKino_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5715278003:AAHBX2f712OSQX1z_kJ77n1I24mTLwFrdWI";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+        var id = user.getId();
+
+        sendText(id, msg.getText());
+
+        if(msg.isCommand()){
+            if(msg.getText().equals("/getFilms"))
+                sendText(id, getFilms());
+        }
+
+    }
+
+    public String getFilms() {
+        return "Gladiator";
+    }
+
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        System.out.println("Hello world!");
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        Bot bot = new Bot();
+        botsApi.registerBot(bot);
+        bot.sendText(1234L, "Hello World!");
+    }
+}
